2771b617078d35dd14fa8c2572fef0fc26e5b99c
Joakim Forslund <syroumusic@gmail.com>
2019-02-22 21:35:37 +0100

    Cleared temp folders

--- src/commonMain/kotlin/com/poehelper/Sample.kt
+++ src/commonMain/kotlin/com/poehelper/Sample.kt
@@ -1,10 +1,16 @@
 package com.poehelper
 
+import com.soywiz.korio.async.async
+import com.soywiz.korio.async.launch
 import com.soywiz.korio.async.runBlockingNoSuspensions
 import com.soywiz.korio.lang.Charsets
 import com.soywiz.korio.lang.toString
 import com.soywiz.korio.net.AsyncClient
+import com.soywiz.korio.net.AsyncSocketFactory
+import com.soywiz.korio.net.createTcpClient
+import com.soywiz.korio.stream.readAll
 import com.soywiz.korio.stream.readAvailable
+import kotlinx.coroutines.GlobalScope
 
 expect fun test(): String
 
@@ -15,7 +21,7 @@ val dataFiles: ByteArray = byteArrayOf(0x03, 0x00, 0x04, 0x44, 0x00, 0x61, 0x00,
 
 fun main(args: Array<String>) = runBlockingNoSuspensions {
     println(test())
-    println("Super test")
+    /*println("Super test")
     val client: AsyncClient = AsyncClient.create(false)
     println("Created async client")
     client.connect("172.65.204.172", 12995)
@@ -26,6 +32,17 @@ fun main(args: Array<String>) = runBlockingNoSuspensions {
         client.write(dataFiles)
         dataToRead = client.readAvailable()
         println(dataToRead.toString(Charsets.UTF8))
-    }
+    }*/
+    connectAndDownloadFiles()
+}
 
+fun connectAndDownloadFiles(){
+    GlobalScope.launch {
+        val tcpClient = createTcpClient("172.65.204.172", 12995, false)
+        if(tcpClient.connected){
+            println("We are connected!")
+            tcpClient.write(dataFiles)
+            val result = tcpClient.readAll()
+        }
+    }
 }
--- src/mingwMain/kotlin/com/poehelper/SampleMingw.kt
+++ src/mingwMain/kotlin/com/poehelper/SampleMingw.kt
@@ -1,5 +1,7 @@
 package com.poehelper
 
+import com.soywiz.korio.net.NativeAsyncSocketFactory
+
 actual fun test(): String {
     return "This is MingW"
 }
